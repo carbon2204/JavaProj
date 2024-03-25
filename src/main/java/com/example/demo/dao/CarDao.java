8b0d5c82fbb7f5bcbd9104ef0ee992adebea2d8a
carbon2204 <dmitry.kuzmin.work@gmail.com>
2024-03-25 11:54:45 +0300

    some tweaks

--- src/main/java/com/example/demo/dao/CarDao.java
+++ src/main/java/com/example/demo/dao/CarDao.java
@@ -1,7 +1,6 @@
 package com.example.demo.dao;
 
 import com.example.demo.model.Car;
-import com.example.demo.model.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import com.example.demo.repository.CarRepository;
