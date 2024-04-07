0b722ee4f829935d13cc72ada841149ad3fb0b86
carbon2204 <dmitry.kuzmin.work@gmail.com>
2024-04-07 17:01:03 +0300

    some tweaks

---
+++ src/main/java/com/example/demo/component/LoggingAspect.java
@@ -0,0 +1,48 @@
+package com.example.demo.component;
+
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+
+/**
+ * The type Logging aspect.
+ */
+@Aspect
+@Component
+public class LoggingAspect {
+  private static final Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
+
+  @Pointcut("execution(* com.example.demo.controllers.*.create*(..))")
+    public void create() {
+
+  }
+
+  @Pointcut("execution(* com.example.demo.controllers.*.update*(..))")
+    public void update() {
+
+  }
+
+  @Pointcut("execution(* com.example.demo.controllers.*.delete*(..))")
+    public void delete() {
+
+  }
+
+  @AfterReturning(pointcut = "create()", returning = "result")
+    public void logCreate(Object result) {
+    logger.info("Created: {}", result);
+  }
+
+  @AfterReturning(pointcut = "update()", returning = "result")
+    public void logUpdate(Object result) {
+    logger.info("Updated: {}", result);
+  }
+
+  @AfterReturning(pointcut = "delete()", returning = "result")
+    public void logDelete(Object result) {
+    logger.info("Deleted: {}", result);
+  }
+}
---
+++ src/main/java/com/example/demo/exeptions/ExceptionsHandler.java
@@ -0,0 +1,67 @@
+package com.example.demo.exeptions;
+
+import lombok.extern.slf4j.Slf4j;
+import org.hibernate.exception.ConstraintViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+/**
+ * The type Exceptions handler.
+ */
+@Slf4j
+@RestControllerAdvice
+public class ExceptionsHandler {
+  @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+  @ExceptionHandler(RuntimeException.class)
+  public ErrorMessage runtimeError(Exception ex) {
+    log.error("Runtime exception", ex);
+    return new ErrorMessage(ex.getMessage());
+  }
+
+  @ResponseStatus(HttpStatus.NOT_FOUND)
+  @ExceptionHandler({ResponseStatusException.class, NoHandlerFoundException.class})
+  public ErrorMessage notFoundException(Exception ex) {
+    log.error("404 NotFound exception");
+    return new ErrorMessage("Resource not found");
+  }
+
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  @ExceptionHandler({
+    HttpClientErrorException.class,
+    HttpMessageNotReadableException.class,
+    MethodArgumentNotValidException.class,
+    MissingServletRequestParameterException.class,
+    ConstraintViolationException.class
+  })
+  public ErrorMessage handleBadRequestException(Exception ex) {
+    log.error("400 error");
+    return new ErrorMessage("Bad request");
+  }
+
+  /**
+     * Handle method not allowed error message.
+     *
+     * @param ex the ex
+     * @return the error message
+    */
+  @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
+  @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+  public ErrorMessage handleMethodNotAllowed(Exception ex) {
+    log.error("MethodNotAllowed exception");
+    return new ErrorMessage("Method not allowed");
+  }
+
+  /**
+     * The type Error message.
+     */
+  public record ErrorMessage(String message) {}
+}
