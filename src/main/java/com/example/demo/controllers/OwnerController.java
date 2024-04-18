b69a95c0b2d2d593be61167e63c06467e312c6e3
carbon2204 <dmitry.kuzmin.work@gmail.com>
2024-04-18 06:57:22 +0300

    some tweaks

--- src/main/java/com/example/demo/controllers/OwnerController.java
+++ src/main/java/com/example/demo/controllers/OwnerController.java
@@ -6,7 +6,6 @@ import com.example.demo.model.Product;
 import com.example.demo.service.OwnerService;
 import jakarta.transaction.Transactional;
 import java.util.List;
-import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
